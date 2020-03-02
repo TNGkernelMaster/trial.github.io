2b251aa2f0b68fd32735ce0ae495161ea965e264
TNGkernelMaster <bech.alexander3@gmail.com>
2020-03-02 23:45:20 +0530

    Updates

---
+++ precache-manifest.5f4fab2f96441dd7d714d866e33f450f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360f39ace83f97d8e0ac2ccfb4ca403a",
+    "url": "/trial/index.html"
+  },
+  {
+    "revision": "e9862361feff8206047f",
+    "url": "/trial/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3149a0ef6aade5db0825",
+    "url": "/trial/static/js/2.e90be324.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/trial/static/js/2.e90be324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9862361feff8206047f",
+    "url": "/trial/static/js/main.488dfccd.chunk.js"
+  },
+  {
+    "revision": "0ea834271862c163b7d3",
+    "url": "/trial/static/js/runtime-main.0168ed22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trial/static/media/logo.5d5d9eef.svg"
+  }
+]);
